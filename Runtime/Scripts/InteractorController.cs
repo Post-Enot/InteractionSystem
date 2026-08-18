2942195a8b158367067052976b3c21da12aa7a62
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2026-08-19 00:33:56 +0300

    Изменён способ инициализации контекста

--- Runtime/Scripts/InteractorController.cs
+++ Runtime/Scripts/InteractorController.cs
@@ -6,13 +6,10 @@ namespace PostEnot.Toolkits.InteractionSystem
     public abstract class InteractorController<TContext, TInteraction> : MonoBehaviour
         where TInteraction : InteractionBase
     {
-        #region Inspector
-        [SerializeField] private TContext context;
-        #endregion
-
         public InteractionMap<TContext, TInteraction> Map => (InteractionMap<TContext, TInteraction>)Interactor.Map;
         public Interactor<TContext, InteractionProvider<TContext, TInteraction>, TInteraction> Interactor { get; private set; }
 
+        protected abstract TContext Context { get; }
         protected abstract IEventBus EventBus { get; }
 
         protected abstract InteractionMap<TContext, TInteraction> InitInteractionMap( InteractionMapBuilder<TContext, TInteraction> builder);
@@ -22,7 +19,7 @@ namespace PostEnot.Toolkits.InteractionSystem
             InteractionMapBuilder<TContext, TInteraction> builder = new();
             InteractionMap<TContext, TInteraction> map = InitInteractionMap(builder);
             IEventReceiver triggerReceiver = EventBus.CreateReceiver();
-            Interactor = new Interactor<TContext, InteractionProvider<TContext, TInteraction>, TInteraction>(context, map, triggerReceiver);
+            Interactor = new Interactor<TContext, InteractionProvider<TContext, TInteraction>, TInteraction>(Context, map, triggerReceiver);
         }
 
         protected virtual void OnEnable() => Interactor.Enable();
